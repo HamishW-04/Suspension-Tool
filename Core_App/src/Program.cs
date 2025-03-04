911cd92f424774dfc7615e7be05721464854b1b2
HamishW-04 <hamishjwormald@gmail.com>
2025-03-04 20:34:10 +0000

    Initial commit

---
+++ Core_App/src/Program.cs
@@ -0,0 +1,53 @@
+﻿//using System.Numerics;
+using Raylib_cs;
+using System.Numerics;
+
+namespace Core_App
+{
+
+    class SceneEntity
+    {
+        //Attributs
+        private protected Transform m_Transform;
+        private Scene m_Scene;
+
+        public SceneEntity(Scene scene)
+        {
+            m_Scene = scene;
+            m_Transform = new Transform();
+        }
+    }
+
+    class Line:SceneEntity
+    {
+        //Attributes
+        private Vector3 m_start;
+        private Vector3 m_end;
+
+        //Constructor
+        public Line(Scene scene) : base(scene)
+        {
+            
+        }
+
+        //Fields
+
+    }
+
+    class Scene
+    {
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Transform Origin = new Transform();
+            Transform P = new Transform(Origin, new Vector3(1,5,2));
+
+
+            Console.WriteLine(P.GetGlobalPosition());
+        }
+    }
+}
---
+++ Core_App/src/Transform.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_App
+{
+    class Transform
+    {
+        //Attributes
+        private Transform? m_Parent;
+        private Vector3 m_Position;
+
+        public Transform()
+        {
+            m_Position = Vector3.Zero;
+        }
+
+        public Transform(Transform Parent, Vector3 LocalPosition)
+        {
+            m_Parent = Parent;
+            m_Position = LocalPosition;
+        }
+
+        //Properties
+        public Vector3 GetGlobalPosition()
+        {
+            if (m_Parent != null) return m_Parent.GetGlobalPosition() + m_Position;
+            else return m_Position;
+        }
+
+        public Vector3 GetLocalPosition() { return m_Position; }
+
+        public void SetLocalPosition(Vector3 NewPosition)
+        {
+            m_Position = NewPosition;
+        }
+
+        //Methods
+        public void Translate(Vector3 Translation)
+        {
+            m_Position += Translation;
+        }
+    }
+}
